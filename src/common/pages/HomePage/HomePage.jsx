7e6022c2423c3a2bafea889d1f93658a4ef7e1e5
Nazar Horshevikov <rangar.danarim@gmail.com>
2023-02-15 20:26:24 +0200

    ADD StartTrial section to HomePage

--- src/common/pages/HomePage/HomePage.jsx
+++ src/common/pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import Posts from "../../components/sections/Posts/Posts";
 import Resources from "../../components/sections/Resources/Resources";
+import StartTrial from "../../components/sections/StartTrial/StartTrial";
 
 const HomePage = () => (
   <main>
     <Resources/>
     <Posts/>
+    <StartTrial/>
   </main>
 );
 
